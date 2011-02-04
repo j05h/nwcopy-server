86ca5e7bfc2499decf2e3e412eff35ce2e4b33eb
Josh Kleinpeter <jkleinpeter@attinteractive.com>
2011-02-04 08:55:58 -0600

    return guid if no filename. small refactor on controller.

--- app/controllers/pastes_controller.rb
+++ app/controllers/pastes_controller.rb
@@ -1,12 +1,7 @@
 class PastesController < ApplicationController
   protect_from_forgery :except => [:create]
   def create
-    data = params[:data]
-    content, filename = if data.respond_to? :read
-      [data.read, data.original_filename]
-    else
-      [data, nil]
-    end
+    content, filename = read_data
     paste = Paste.new :content => content, :filename => filename
     paste.save
     render :text => "http://#{request.host_with_port}/pastes/#{paste.guid}", :status => 201
@@ -14,7 +9,17 @@ class PastesController < ApplicationController
 
   def show
     paste = Paste.find_by_guid params[:id]
-    headers['Location'] = paste.filename
+    headers['Location'] = paste.filename || paste.guid
     send_data paste.content
+  end
+
+  private
+  def read_data
+    data = params[:data]
+    if data.respond_to? :read
+      [data.read, data.original_filename]
+    else
+      [data, nil]
+    end
   end
 end
--- test/functional/pastes_controller_test.rb
+++ test/functional/pastes_controller_test.rb
@@ -1,21 +1,31 @@
 require 'test_helper'
 
 class PastesControllerTest < ActionController::TestCase
-  test 'takes a copy' do
+  test 'copies text' do
     assert_difference 'Paste.count' do
       post :create, {"data" => "this is the body i'm sending"}
     end
     assert_match /http:\/\/test.host\/pastes\/\w*/, response.body
   end
 
-  test 'takes a binary file' do
+  test 'copies a binary file' do
     assert_difference 'Paste.count' do
       post :create, :data => fixture_file('/rails.png', 'image/png')
     end
     assert_equal 'rails.png', Paste.all.first.filename
   end
 
-  test 'pastes the data' do
+  test 'pastes text data' do
+    paste = Paste.new :content => "this is some text"
+    paste.save
+
+    get :show, :id => paste.guid
+
+    assert_equal paste.content, response.body
+    assert_equal paste.guid, response.headers['Location']
+  end
+
+  test 'pastes binary data' do
     paste = binary_paste
     get :show, :id => paste.guid
     assert_equal paste.content, response.body
