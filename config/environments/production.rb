2514247fe0c9432d9c49553ed9f09f9fc416d717
Josh Kleinpeter <jkleinpeter@attinteractive.com>
2011-02-05 13:33:04 -0600

    Updated access keys for github.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,6 @@ NwcopyServer::Application.configure do
   config.logger    = Logger.new STDOUT
   config.log_level = :info
 
-  GITHUBACCESS = '701aa6c7863e2f650be2'
-  GITHUBSECRET = '2fc16398de8fe58899089759394ecdb899874a3f'
+  GITHUBACCESS = 'f6a874390ff78672a9e9' #'701aa6c7863e2f650be2'
+  GITHUBSECRET = 'd27d40b122b9b5bf49720171ccf1ad9d8d5742d9' #'2fc16398de8fe58899089759394ecdb899874a3f'
 end
