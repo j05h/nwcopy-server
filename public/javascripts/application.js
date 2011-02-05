8e7382977ec704b811fb051c289b00296519fb95
j05h <josh@kleinpeter.org>
2011-02-05 09:58:21 -0600

    Improved instructions.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,12 +1,30 @@
 $(document).ready(function() {
+  $('#pastes_open').bind('click', function(event) {
+    $('#pastes').slideToggle();
+    $('#instructions').slideUp();
+  });
+  $('#creds').bind('click', function(event) {
+    $('#instructions').slideDown();
+    $('#pastes').slideUp();
+  });
+  $('#instructions_open').bind('click', function(event) {
+    $('#instructions').slideToggle();
+    $('#pastes').slideUp();
+  });
   $('#curl_open').bind('click', function(event) {
     $('#curl').slideToggle();
+    $('#ruby').slideUp();
+    $('#bash').slideUp();
   });
   $('#ruby_open').bind('click', function(event) {
     $('#ruby').slideToggle();
+    $('#bash').slideUp();
+    $('#curl').slideUp();
   });
   $('#bash_open').bind('click', function(event) {
     $('#bash').slideToggle();
+    $('#ruby').slideUp();
+    $('#curl').slideUp();
   });
 });
 
